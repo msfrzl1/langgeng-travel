d65c9a865ae24e1f92829259c4a7da4cd69822ba
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-07-28 14:30:28 +0700

    create register page in pages, connect router

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,8 +1,13 @@
 import LoginPage from "../pages/login";
+import RegisterPage from "../pages/register";
 
 export const routeList = [
   {
     path: "/login",
     element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
   },
 ];
