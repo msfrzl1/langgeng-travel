57c8353f49d4003d6784050942f7ad10f6b6696e
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-08-21 10:13:47 +0700

    Add ToastContainer to LoginPage for displaying toast notifications

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,10 +1,23 @@
 import FormLogin from "../../components/Fragments/FormLogin";
 import AuthLayout from "../../components/Layouts/AuthLayout";
+import { ToastContainer } from "react-toastify";
 
 export default function LoginPage() {
   return (
     <AuthLayout title={"Form Login"} type={"login"}>
       <FormLogin />
+      <ToastContainer
+        position="top-center"
+        autoClose={2300}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </AuthLayout>
   );
 }
