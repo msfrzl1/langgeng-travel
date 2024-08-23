342d47c4f26d3cecf5b8785c523df6f195e661a1
msfrzl1 <muhammadsaifurrizal14@gmail.com>
2024-08-23 17:31:48 +0700

    Added toggle functionality for password fields.

--- src/components/Fragments/FormRegistration/index.jsx
+++ src/components/Fragments/FormRegistration/index.jsx
@@ -6,10 +6,16 @@ import SelecOption from "../../Elements/SelectOption";
 import HidePasswordToggle from "../../Elements/HidePasswordToggle";
 
 export default function FormRegister() {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswords, setShowPasswords] = useState({
+    password: false,
+    passwordRepeat: false,
+  });
 
-  const handleShowPassword = () => {
-    setShowPassword((prevState) => !prevState);
+  const toggleShowPassword = (key) => {
+    setShowPasswords((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
   };
 
   return (
@@ -17,81 +23,83 @@ export default function FormRegister() {
       <div className="grid w-full grid-cols-1 gap-3 md:grid-cols-2">
         <div className="w-full">
           <FormInput
-            id={"email"}
-            htmlFor={"email"}
-            name={"email"}
-            label={"Email"}
-            type={"email"}
-            placeholder={"example@gmail.com"}
+            id="email"
+            htmlFor="email"
+            name="email"
+            label="Email"
+            type="email"
+            placeholder="example@gmail.com"
           />
           <Gap y={0.8} />
           <FormInput
-            id={"name"}
-            htmlFor={"name"}
-            name={"name"}
-            label={"Name"}
-            type={"text"}
-            placeholder={"Enter your Name"}
+            id="name"
+            htmlFor="name"
+            name="name"
+            label="Name"
+            type="text"
+            placeholder="Enter your Name"
           />
           <Gap y={0.8} />
           <div className="relative w-full">
             <FormInput
-              id={"password"}
-              htmlFor={"password"}
-              name={"password"}
-              label={"Password"}
-              type={showPassword ? "text" : "password"}
-              placeholder={"*******"}
+              id="password"
+              htmlFor="password"
+              name="password"
+              label="Password"
+              type={showPasswords.password ? "text" : "password"}
+              placeholder="Enter your password"
             />
             <HidePasswordToggle
-              type={"button"}
-              showPassword={showPassword}
-              handleShowPassword={handleShowPassword}
+              type="button"
+              showPassword={showPasswords.password}
+              handleShowPassword={() => toggleShowPassword("password")}
             />
           </div>
           <Gap y={0.8} />
-          <FormInput
-            id={"passwordRepeat"}
-            htmlFor={"passwordRepeat"}
-            name={"passwordRepeat"}
-            label={"Repeat Password"}
-            type={showPassword ? "text" : "password"}
-            placeholder={"*******"}
-          />
+          <div className="relative w-full">
+            <FormInput
+              id="passwordRepeat"
+              htmlFor="passwordRepeat"
+              name="passwordRepeat"
+              label="Repeat Password"
+              type={showPasswords.passwordRepeat ? "text" : "password"}
+              placeholder="Enter your repeat password"
+            />
+            <HidePasswordToggle
+              type="button"
+              showPassword={showPasswords.passwordRepeat}
+              handleShowPassword={() => toggleShowPassword("passwordRepeat")}
+            />
+          </div>
         </div>
         <div className="w-full">
-          <SelecOption
-            id={"role"}
-            htmlFor={"role"}
-            name={"role"}
-            label={"Role"}
-          >
+          <SelecOption id="role" htmlFor="role" name="role" label="Role">
             <option value="admin">Admin</option>
             <option value="user">User</option>
           </SelecOption>
           <Gap y={0.8} />
           <FormInput
-            id={"profilePictureUrl"}
-            htmlFor={"profilePictureUrl"}
-            name={"profilePictureUrl"}
-            label={"Profile Picture Url"}
-            type={"file"}
+            id="profilePictureUrl"
+            htmlFor="profilePictureUrl"
+            name="profilePictureUrl"
+            label="Profile Picture Url"
+            type="file"
           />
           <Gap y={0.8} />
           <FormInput
-            id={"phoneNumber"}
-            htmlFor={"phoneNumber"}
-            name={"phoneNumber"}
-            label={"Phone Number"}
-            type={"number"}
-            placeholder={"08xx-xxxx-xxxx"}
+            id="phoneNumber"
+            htmlFor="phoneNumber"
+            name="phoneNumber"
+            label="Phone Number"
+            type="number"
+            placeholder="08xx-xxxx-xxxx"
           />
         </div>
       </div>
       <Gap y={1} />
       <Button
-        type={"submit"}
-        value={"Registration"}
+        type="submit"
+        value="Registration"
         classname="w-full bg-indigo-500 py-2 hover:bg-indigo-700"
       />
     </form>
